14486ea7eb93fac49dacf22b1d4bb22ac714625d
mahmoud elnaggar <mahmouddnnaggar@gmail.com>
2024-11-03 06:55:07 +0200

    update some logic

--- script.js
+++ script.js
@@ -10,19 +10,47 @@ const highScore = document.querySelector('.highscore');
 const displayNumber = document.querySelector('.number');
 
 // ^ Variables
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let scoreNumber = 20;
-let highScoreNumber = +localStorage.getItem('highScore') || 0;
+let scoreNumber = setScoreNumber(20);
+let highScoreNumber = setHighScore();
+let secretNumber = setSecretNumber();
+let lastSecretNumber;
 
-// ^ functions
-function reset() {
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+// ^ Utility Functions
+function setMessage(msg) {
+  message.textContent = msg;
+}
+function setSecretNumber(reset = false, toWhat = 20) {
+  if (!reset) return Math.trunc(Math.random() * toWhat) + 1;
+  secretNumber = Math.trunc(Math.random() * toWhat) + 1;
+}
+function setHighScore(reset = false, newScore) {
+  if (!reset) return +localStorage.getItem('highScore') || 0;
+  if (newScore && newScore > highScoreNumber) {
+    localStorage.setItem('highScore', JSON.stringify(newScore));
+  }
   highScoreNumber = +localStorage.getItem('highScore') || 0;
   highScore.textContent = highScoreNumber;
-  scoreNumber = 20;
-  score.textContent = scoreNumber;
+}
+function setScoreNumber(number) {
+  if (number) {
+    score.textContent = number;
+    return number;
+  } else {
+    scoreNumber--;
+    score.textContent = scoreNumber;
+  }
+}
+
+// ^ Main functions
+function reset() {
+  setSecretNumber(true);
+  while (secretNumber === +localStorage.getItem('lastSecretNumber')) {
+    setSecretNumber(true);
+  }
+  setHighScore(true);
+  setMessage('Start guessing...');
+  scoreNumber = setScoreNumber(20);
   displayNumber.textContent = '?';
-  message.textContent = 'Start guessing...';
   checkBtn.disabled = false;
   guessNumberInput.value = '';
   document.body.style.backgroundColor = '#222';
@@ -31,25 +59,22 @@ function reset() {
 function tryGuess() {
   const success = () => {
     document.body.style.backgroundColor = '#117554';
-    message.textContent = '🎉 Success';
-    // guessNumberInput.value = '';
+    setMessage('🎉 Success');
     displayNumber.style.width = '30rem';
     displayNumber.textContent = secretNumber;
     // ^ high score update
-    if (scoreNumber > highScoreNumber) highScoreNumber = scoreNumber;
-    highScore.textContent = highScoreNumber;
-    localStorage.setItem('highScore', JSON.stringify(highScoreNumber));
+    setHighScore(true, scoreNumber);
+    // ^ save last secret number
+    localStorage.setItem('lastSecretNumber', guessNumberInput.value);
   };
   const failure = () => {
-    scoreNumber--;
-    score.textContent = scoreNumber;
+    setScoreNumber();
     if (scoreNumber === 0) {
-      message.textContent = '💥 Game Over';
+      setMessage('💥 Game Over');
       document.body.style.backgroundColor = '#D10363';
       checkBtn.disabled = true;
     } else {
-      message.textContent =
-        guessNumber > secretNumber ? '📈 Too High!' : '📉 Too Low';
+      setMessage(guessNumber > secretNumber ? '📈 Too High!' : '📉 Too Low');
     }
   };
   const guessNumber = Number(guessNumberInput.value);
@@ -60,7 +85,7 @@ function tryGuess() {
       failure();
     }
   } else {
-    message.textContent = '🔢 Enter A Number!';
+    setMessage('🔢 Enter A Number!');
   }
 }
 function guessInputHandler() {
